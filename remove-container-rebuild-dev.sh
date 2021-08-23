7fc97528021dea7f5f1b89341dd22cf6a0423c70
domorelivelonger <60047304+domorelivelonger@users.noreply.github.com>
2021-08-23 12:29:56 +0000

    Delete remove-container-rebuild-dev.sh

--- remove-container-rebuild-dev.sh
+++
@@ -1,5 +0,0 @@
-docker rm node -f
-docker rmi idepnetwork/testnet-denali
-docker build . -t idepnetwork/testnet-denali
-docker push idepnetwork/testnet-denali
-docker run --name node -itd idepnetwork/testnet-denali
